d36e542fee0a8f0a0007d53fe8d385b84d3a6e49
SantoshDhakal <clab2080@gmail.com>
2024-06-18 08:50:56 +0545

    Inserted input1 and input2

--- calculator.py
+++ calculator.py
@@ -1,5 +1,20 @@
 
 #We will create a calculator here.
+input1= int(input("Enter number 1"))
+input2= int(input("Enter number 2"))
+
+print add_numbers(input1,input2)
+
+print subtract_numbers(input1,input2)
+
+print multiply_numbers(input1,input2)
+
+print divide_numbers(input1,input2)
+
+print mod_numbers(input1,input2)
+
+print power_numbers(input1,input2)
+
 
 #Add two numbers.
 
