ede2357f50e0eb1d9a1438ba1665e003923d9848
SantoshDhakal <clab2080@gmail.com>
2024-06-18 08:47:10 +0545

    Assigned tasks

--- calculator.py
+++ calculator.py
@@ -1,3 +1,19 @@
 
 #We will create a calculator here.
 
+#Add two numbers.
+
+
+#Subtract two numbers
+
+
+#Multiply two numbers
+
+
+#Divide two numbers
+
+
+#Perfom a mod b
+
+#Perform a power b
+
