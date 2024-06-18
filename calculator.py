4b04a56e5e33d913ac36ed705b6488b5ae5b088a
santosh-pkr <clab2080@gmail.com>
2024-06-18 21:27:30 +0545

    Create a function add_numbers(a,b)

--- calculator.py
+++ calculator.py
@@ -5,6 +5,7 @@ input2= int(input("Enter number 2"))
 
 print add_numbers(input1,input2)
 
+
 print subtract_numbers(input1,input2)
 
 print multiply_numbers(input1,input2)
@@ -17,7 +18,8 @@ print power_numbers(input1,input2)
 
 
 #Add two numbers.
-
+def add_numbers(a,b):
+  return a+b
 
 #Subtract two numbers
 
